53d2b939116564c80f15aa34bc88b91c443772c6
Tilingo <nestirco@gmail.com>
2018-06-07 14:05:27 -0400

    index.hbs and index route for menu

--- routes/menuController.js
+++ routes/menuController.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const Menu = require('../models/Menu')
+const Section = require('../models/MenuSection')
+const Item = require('../models/MenuItem')
 
 
 router.get('/', function(req, res, next) {
-  res.send("It's alive");
+  Menu
+    .find()
+    .then((listOfMenus) => {
+      res.render('menu/index', {
+        title: "Menus",
+        listOfMenus
+      })
+    })
+    .catch((err) => res.send(err))
 });
 
 module.exports = router;
